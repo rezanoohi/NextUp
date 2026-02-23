42d52a7e28e60ab553b5d1df4ed9a41269796212
Reza Noohi <w.rezanoohi@gmail.com>
2026-02-23 16:51:57 +0330

    fix: gracefully handle missing .env file in production

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,5 @@
-process.loadEnvFile('.env');
+try {
+    process.loadEnvFile('.env');
+} catch (err) {}
 
 export const {SERVER_PORT, DB_URL, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, ACCESS_TOKEN_EXPIRES_IN, REFRESH_TOKEN_EXPIRES_IN} = process.env
